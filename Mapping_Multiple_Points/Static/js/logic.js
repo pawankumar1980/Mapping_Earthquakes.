0f910875e55ba003dda2998d03600f440f1995c5
pawankumar1980 <111800568+pawankumar1980@users.noreply.github.com>
2022-11-27 16:37:26 -0500

    changes to the branch

--- Mapping_Multiple_Points/Static/js/logic.js
+++ Mapping_Multiple_Points/Static/js/logic.js
@@ -49,4 +49,9 @@ let cities = [{
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
